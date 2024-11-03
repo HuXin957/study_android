35d9c1b4596f4bb038127439eb6e684f6957225e
huqiang <huqiang13123@163.com>
2024-11-03 21:10:06 +0800

    静态fragment

---
+++ app/src/main/java/com/example/study_android/fragment/StaticFragment.java
@@ -0,0 +1,25 @@
+package com.example.study_android.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.study_android.R;
+
+public class StaticFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_static, container, false);
+    }
+}
